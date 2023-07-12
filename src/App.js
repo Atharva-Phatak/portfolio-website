1923f97b6a5cb180927d2249ab3c4a66eb5aaeea
Atharva-Phatak <athp456@gmail.com>
2023-07-11 23:03:46 -0400

    remove side-bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 import NavBar from "./Components/NavBar";
 import HeroSection from "./Components/HeroSection";
-import Social from "./Components/Social";
 import Projects from "./Components/Projects";
 import About from "./Components/About";
 import Skills from "./Components/Skills";
 import Footer from "./Components/Footer";
 
 function App() {
-  //const [isMenuDisplayed, setMenuDisplay] = useState(false)
+  const [isMenuDisplayed, setMenuDisplay] = useState(false)
   return (
     <div>
       
-      {/*<NavBar isMenuDisplayed={isMenuDisplayed}  setMenuDisplay = {setMenuDisplay}/> */}
+      <NavBar isMenuDisplayed={isMenuDisplayed}  setMenuDisplay = {setMenuDisplay}/>
       <HeroSection />  
-      <Social /> 
+      {/*<Social /> */}
       <About/>
       <Projects />
       <Skills /> 
